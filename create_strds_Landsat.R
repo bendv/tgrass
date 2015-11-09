27ce44e3e0aaf0f552b8a318e4d1ffe25eb8271a
Ben <devries.br@gmail.com>
2015-11-09 14:11:02 +0100

    Update create_strds_Landsat.R

--- create_strds_Landsat.R
+++ create_strds_Landsat.R
@@ -10,7 +10,7 @@
 ##
 ## parameters:
 ##    - search_path: path containing Landsat scene folders
-##    - pattern: pattern indicating which band to use (e.g. 'band1', 'NDVI', etc...)
+##    - pattern: pattern indicating which band to use. For spectral bands, this should be one of 'blue', 'green', 'red', 'NIR', 'SWIR1', or 'SWIR2', but not the band numbers (since these are different between L5/7 and L8). Otherwise, other metrics can like 'NDVI', 'NDMI', 'NBR', 'NBR2', 'SAVI', 'MSAVI', or 'EVI', can be used
 ##    - strds: name of the strds to be created
 ##    - title: title of the strds
 ##    - description: description of the strds
@@ -42,20 +42,20 @@ strds <- args[3]
 title <- sprintf("\'%s\'", args[4])
 desc <- sprintf("\'%s\'", args[5])
 cpus <- as.numeric(args[6])
-if(length(args) > 6 & args[7] == "overwrite") {
+if(length(args) > 6 & args[7] == 'overwrite') {
   overwrite = TRUE
 } else {
   overwrite = FALSE
 }
 
-cat("\n#############################\n")
-cat("Creating ", strds, " and registering all ", pattern, " scenes in ", path, ".\n", sep = "")
-cat("#############################\n\n")
+cat('\n#############################\n')
+cat('Creating ', strds, ' and registering all ', pattern, ' scenes in ', path, '.\n', sep = '')
+cat('#############################\n\n')
 
 library(bfastSpatial)
-library(spgrass6)
+library(spgrass7)
 setwd(path)
-loc <- Sys.setlocale("LC_TIME", "en_US.utf-8")
+loc <- Sys.setlocale('LC_TIME', 'en_US.utf-8')
 
 # create the strds
 if(overwrite) {
@@ -64,13 +64,6 @@ if(overwrite) {
   command <- sprintf("t.create type=strds temporaltype=absolute output=%s title=%s description=%s", strds, title, desc)
 }
 system(command)
-
-# list scene folders and scene info
-fl <- list.files()
-s <- getSceneinfo(fl)
-
-# format dates for r.datestamp
-dates <- tolower(format(s$date, format = "%d %b %Y"))
 
 # function for batch import and timestamping of raster maps
 r.in.gdal.timestamp <- function(r, name, date) {
@@ -82,22 +75,51 @@ r.in.gdal.timestamp <- function(r, name, date) {
   execGRASS("r.timestamp", parameters = list(map = name, date = date))
 }
 
-# list all .tif files (with pattern (e.g. all blue band))
-srch <- glob2rx(sprintf("*%s*.tif", pattern))
-files <- list.files(pattern = srch, recursive = TRUE)
+# convert band 'tag' if spectral bands, otherwise use literal search
+if(pattern %in% c("blue", "green", "red", "NIR", "SWIR1", "SWIR2")) {
+  
+  lut <- data.frame(band = c("blue", "green", "red", "NIR", "SWIR1", "SWIR2"),
+                    TMETM = sprintf("band%s", c(1:5, 7)),
+                    OLI = sprintf("band%s", c(2:7)),
+                    stringsAsFactors = FALSE)
+  
+  srch1 <- glob2rx(sprintf("*%s*.tif", lut$TMETM[lut$band == pattern]))
+  srch2 <- glob2rx(sprintf("*%s*.tif", lut$OLI[lut$band == pattern]))
+  fl1 <- list.files(pattern = srch1, recursive = TRUE)
+  s1 <- getSceneinfo(fl1)
+  fl1 <- fl1[s1$sensor %in% c("TM", "ETM+ SLC-on", "ETM+ SLC-off")]
+  fl2 <- list.files(pattern = srch2, recursive = TRUE)
+  s2 <- getSceneinfo(fl2)
+  fl2 <- fl2[s2$sensor == "OLI"]
+  fl <- c(fl1, fl2)
+  
+} else {
+  
+  srch <- glob2rx(sprintf("*%s*.tif", pattern))
+  fl <- list.files(pattern = srch, recursive = TRUE)
+  
+}
+
+# get scene info and sort by date
+s <- getSceneinfo(fl)
+fl <- fl[order(s$date)]
+s <- getSceneinfo(fl)
+
+# format dates for r.datestamp
+dates <- tolower(format(s$date, format = "%d %b %Y"))
 
 # apply function over all scenes
 if(cpus == 1) {
   for(i in 1:length(fl)) {
     label <- sprintf("%s_%s", row.names(s)[i], pattern)
-    r.in.gdal.timestamp(files[i], label, dates[i])
+    r.in.gdal.timestamp(fl[i], label, dates[i])
   }
 } else if(cpus > 1) {
   library(doMC)
   registerDoMC(cores = cpus)
   junk <- foreach(i = 1:length(fl)) %dopar% {
     label <- sprintf("%s_%s", row.names(s)[i], pattern)
-    r.in.gdal.timestamp(files[i], label, dates[i])
+    r.in.gdal.timestamp(fl[i], label, dates[i])
   }
 }
 
